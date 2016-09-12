ae3892edd29711fea245f9cad749cbd543d188f4
eitchtee <netotrotta@gmail.com>
2016-09-11 21:50:04 -0300

    Adds Linux support
    Uses the Spotify's MPRIS API to be able to get the song and artist name from Spotify on Linux.
    
    Read more about it here: http://stackoverflow.com/questions/33883360/get-spotify-currently-playing-track

--- spotilib.py
+++ spotilib.py
@@ -1,94 +1,141 @@
-import win32gui
-import win32api
 import os
+import platform
+if platform.system() == 'Windows':
+    import win32gui
+    import win32api
+elif platform.system() == 'Linux':
+    import dbus
 
 ###Virtual-KeyCodes###
 Media_Next = 0xB0
 Media_Previous = 0xB1
-Media_Pause = 0xB3 ##Play/Pause
+Media_Pause = 0xB3  # Play/Pause
 Media_Mute = 0xAD
 
 
-
 ###SpotifyInfo###
+def song_info_linux():
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                     "/org/mpris/MediaPlayer2")
+    spotify_properties = dbus.Interface(spotify_bus,
+                                    "org.freedesktop.DBus.Properties")
+    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    song_info = metadata['sexam:title']
+    return song_info
+
+
+def artist_info_linux():
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                     "/org/mpris/MediaPlayer2")
+    spotify_properties = dbus.Interface(spotify_bus,
+                                    "org.freedesktop.DBus.Properties")
+    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    artist_info = metadata['xesam:artist'][0]
+    return artist_info
+
+
 def getwindow(Title="SpotifyMainWindow"):
-	window_id = win32gui.FindWindow(Title, None)
-	return window_id
-	
+    window_id = win32gui.FindWindow(Title, None)
+    return window_id
+
+
 def song_info():
-	try:
-		song_info = win32gui.GetWindowText(getwindow())
-	except:
-		pass
-	return song_info
+    try:
+        song_info = win32gui.GetWindowText(getwindow())
+    except:
+        pass
+    return song_info
+
 
 def artist():
-	try:
-		temp = song_info()
-		artist, song = temp.split("-",1)
-		artist = artist.strip()
-		return artist
-	except:
-		return "There is noting playing at this moment"
-	
+    if platform.system() == 'Windows':
+        try:
+            temp = song_info()
+            artist, song = temp.split("-", 1)
+            artist = artist.strip()
+            return artist
+        except:
+            return "There is nothing playing at this moment"
+    elif platform.system() == 'Linux':
+        try:
+            return artist_info_linux()
+        except DBusException:
+            return "There is nothing playing at this moment"
+
+
 def song():
-	try:
-		temp = song_info()
-		artist, song = temp.split("-",1)
-		song = song.strip()
-		return song
-	except:
-		return "There is noting playing at this moment"
-	
+    if platform.system() == 'Windows': 
+        try:
+            temp = song_info()
+            artist, song = temp.split("-", 1)
+            song = song.strip()
+            return song
+        except:
+            return "There is nothing playing at this moment"
+    elif platform.system() == 'Linux':
+        try:
+            return artist_info_linux()
+        except DBusException:
+            return "There is nothing playing at this moment"
+
 ###SpotifyBlock###
+
+
 def createfolder(folder_path="C:\SpotiBlock"):
-	if not os.path.exists(folder_path):
-		os.makedirs(folder_path)
-	else:
-		pass
-	
-def createfile(file_path="C:\SpotiBlock\Block.txt" ):
-	if not os.path.exists(file_path):
-		file = open(file_path, "a")
-		file.write("ThisFirstLineWillBeIgnoredButIsNecessaryForUse")
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    else:
+        pass
+
+
+def createfile(file_path="C:\SpotiBlock\Block.txt"):
+    if not os.path.exists(file_path):
+        file = open(file_path, "a")
+        file.write("ThisFirstLineWillBeIgnoredButIsNecessaryForUse")
+
 
 def blocklist(file_path="C:\SpotiBlock\Block.txt"):
-	block_list = []
-	for line in open(file_path, "r"):
-		if not line == "":
-			block_list.append(line.strip())
-	return block_list
-	
-def add_to_blocklist(file_path="C:\SpotiBlock\Block.txt"):
-	with open(file_path, 'a') as text_file:
-		text_file.write("\n" + song_info())
-		
-def reset_blocklist(file_path="C:\SpotiBlock\Block.txt"):
-	with open(file_path, 'w') as text_file:
-		text_file.write("ThisFirstLineWillBeIgnored")
-		pass
-	
-	
+    block_list = []
+    for line in open(file_path, "r"):
+        if not line == "":
+            block_list.append(line.strip())
+    return block_list
 
-	
+
+def add_to_blocklist(file_path="C:\SpotiBlock\Block.txt"):
+    with open(file_path, 'a') as text_file:
+        text_file.write("\n" + song_info())
+
+
+def reset_blocklist(file_path="C:\SpotiBlock\Block.txt"):
+    with open(file_path, 'w') as text_file:
+        text_file.write("ThisFirstLineWillBeIgnored")
+        pass
+
+
 ###Media Controls###
 def hwcode(Media):
-	hwcode = win32api.MapVirtualKey(Media, 0)
-	return hwcode
+    hwcode = win32api.MapVirtualKey(Media, 0)
+    return hwcode
+
 
 def next():
-	win32api.keybd_event(Media_Next, hwcode(Media_Next))
-	
+    win32api.keybd_event(Media_Next, hwcode(Media_Next))
+
+
 def previous():
-	win32api.keybd_event(Media_Previous, hwcode(Media_Previous))
-	
+    win32api.keybd_event(Media_Previous, hwcode(Media_Previous))
+
+
 def pause():
-	win32api.keybd_event(Media_Pause, hwcode(Media_Pause))
-	
+    win32api.keybd_event(Media_Pause, hwcode(Media_Pause))
+
+
 def play():
-	win32api.keybd_event(Media_Pause, hwcode(Media_Pause))
-	
+    win32api.keybd_event(Media_Pause, hwcode(Media_Pause))
+
+
 def mute():
-	win32api.keybd_event(Media_Mute, hwcode(Media_Mute))
-	
-	
+    win32api.keybd_event(Media_Mute, hwcode(Media_Mute))
